87183c5814fa22c16fac0cdffbe609be0e230ea9
KuicsCristian <kuics.cristian@gmail.com>
2019-05-08 18:57:44 +0300

    updated files
    corrected some paths

--- header.php
+++ header.php
@@ -3,8 +3,7 @@
 	<head>
 
 		<?php wp_head(); ?>
-		<link href="<?php echo get_stylesheet_uri(); ?>/layout.css" rel="stylesheet">
-		<link href="<?php echo get_stylesheet_uri(); ?>/reset.css" rel="stylesheet">
+		<link href="<?php echo get_stylesheet_uri(); ?>" rel="stylesheet">
 
 	</head>
 <div class="header">
